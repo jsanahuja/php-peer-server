a208449ea6f5b5c10e406129d2f0f834a32a272a
Javier Sanahuja <bannss1@gmail.com>
2020-09-18 00:22:12 +0200

    Max 10 members by defualt in the example configuration

--- config.example.php
+++ config.example.php
@@ -5,7 +5,7 @@ define("LOG_PATH",  __DIR__ . "/logs/server.log");
 define("CERT_CA",  "path to your cert");
 define("CERT_KEY", "path to you private key");
 define("ROOM_HASH_LENGTH", 32);
-define("ROOM_MAX_CLIENTS", 40);
+define("ROOM_MAX_CLIENTS", 10);
 
 // Private key to store room passwords.
 // Generate it using sodium_bin2hex(random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES))
